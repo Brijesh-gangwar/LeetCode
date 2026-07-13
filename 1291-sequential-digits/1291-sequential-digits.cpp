4556b7490d32d1c8e2826267e17d0c26efe761fd
Brijesh Gangwar <130545652+Brijesh-gangwar@users.noreply.github.com>
2026-07-13 22:41:34 +0530

    Time: 2 ms (6.7%), Space: 8.2 MB (97.69%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,83 +1,24 @@
 #include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
         vector<int> ans;
 
-        string s = to_string(low);
+        for (int len = 2; len <= 9; len++) {
 
-        int nl = s.size();
+            for (int start = 1; start + len - 1 <= 9; start++) {
 
-        // find first val >= low , in sequence
-        int digit = s[0] - '0';
-        string temp = "";
+                int num = 0;
 
-        while (temp.size() != s.size() && digit <= 9) {
-            temp = temp + to_string(digit);
+                for (int d = start; d < start + len; d++) {
+                    num = num * 10 + d;
+                }
 
-            if (temp.size() == nl && stoi(temp) < low)
-                temp.erase(0, 1);
-
-            digit++;
-        }
-
-
-        if (temp.size() != nl)
-            temp = "";
-
-
-        // insert value to ans
-        while (!temp.empty() && stoi(temp) >= low && stoi(temp) <= high) {
-            ans.push_back(stoi(temp));
-
-            // remove first char
-            temp.erase(0, 1);
-
-            // insert new dig at last
-            int dig = temp[temp.size() - 1] - '0';
-            dig++;
-
-            if (dig <= 9)
-                temp = temp + to_string(dig);
-            else
-                break;
-        }
-
-
-        // Handle lengths greater than low's length
-        int len = nl + 1;
-
-        while (len <= to_string(high).size()) {
-
-            // Generate first sequential number of this length
-            string temp = "";
-            int digit = 1;
-
-            while (temp.size() < len && digit <= 9) {
-                temp += to_string(digit);
-                digit++;
-            }
-
-            // Slide the window
-            while (temp.size() == len) {
-
-                int num = stoi(temp);
-
-                if (num > high)
-                    break;
-
-                if (num >= low)
+                if (num >= low && num <= high)
                     ans.push_back(num);
-
-                // Can't generate next sequential number
-                if (temp.back() == '9')
-                    break;
-
-                temp.erase(0, 1);
-                temp += char(temp.back() + 1);
             }
-
-            len++;
         }
 
         return ans;
