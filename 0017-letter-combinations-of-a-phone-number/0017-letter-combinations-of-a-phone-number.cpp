71c56561906c620bbfb14ab407aeffd0a78558c9
Brijesh Gangwar <130545652+Brijesh-gangwar@users.noreply.github.com>
2026-07-16 00:16:04 +0530

    Time: 0 ms (100%), Space: 9.5 MB (31.38%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,41 +1,44 @@
 class Solution {
 public:
-    void generate_map(unordered_map<char, vector<char>> &mp) {
-        mp['2'] = {'a', 'b', 'c'};
-        mp['3'] = {'d', 'e', 'f'};
-        mp['4'] = {'g', 'h', 'i'};
-        mp['5'] = {'j', 'k', 'l'};
-        mp['6'] = {'m', 'n', 'o'};
-        mp['7'] = {'p', 'q', 'r', 's'};
-        mp['8'] = {'t', 'u', 'v'};
-        mp['9'] = {'w', 'x', 'y', 'z'};
-    }
+
+    vector<string> digitToLetters = {
+            "",
+            "",
+            "abc",  // 2
+            "def",  // 3
+            "ghi",  // 4
+            "jkl",  // 5
+            "mno",  // 6
+            "pqrs", // 7
+            "tuv",  // 8
+            "wxyz"  // 9
+        };
+
+
 
     void fxn(string &digits, vector<string> &ans,
-             unordered_map<char, vector<char>> &mp, int i, string str) {
+        int i, string str) {
         if (i == digits.size()){
 ans.push_back(str);
              return;
         }
 
-        vector<char> arr = mp[digits[i]];
+        int dig = digits[i]-'0';
+        string letters = digitToLetters[dig];
 
-        for (char ch : arr) {
-            fxn(digits, ans, mp, i + 1, str + ch);
+        for (char ch : letters) {
+            fxn(digits, ans, i + 1, str + ch);
         }
     }
 
     vector<string> letterCombinations(string digits) {
-        unordered_map<char, vector<char>> mp;
-
+        
         vector<string> ans;
 
         int i = 0;
         string str = "";
 
-        generate_map(mp);
-
-        fxn(digits, ans, mp, i, str);
+        fxn(digits, ans, i, str);
 
         return ans;
     }
