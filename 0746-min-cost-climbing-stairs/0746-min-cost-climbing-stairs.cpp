cbf4e46b907c071ce2a74413ca6e1bc1f87cbd56
Brijesh Gangwar <130545652+Brijesh-gangwar@users.noreply.github.com>
2026-07-22 01:38:58 +0530

    Time: 0 ms (100%), Space: 17.8 MB (29.42%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     int fxn(vector<int>& cost, int i , vector<int> &dp) {
+        // base case
         if (i == 0 || i== 1)
             return cost[i];
         
+        // check dp state exists or not
         if(dp[i]!= -1) return dp[i];
 
+        // store state in dp and return state
         dp[i] =  cost[i] + min(fxn(cost, i - 1 , dp),fxn(cost, i - 2 , dp));
         return dp[i];
     }
@@ -14,8 +17,18 @@ public:
         int n = cost.size();
 
         vector<int> dp(n+1,-1);
+        
+        dp[0] = cost[0];
+        dp[1] = cost[1];
 
-        return min(fxn(cost, n - 1 ,dp),
-                   fxn(cost, n - 2,dp));
+        for(int i = 2 ; i < n;i++){
+            dp[i] = cost[i] +  min( dp[i-1] , dp[i-2] );
+            
+        }
+
+        return min(dp[n-1] , dp[n-2]);
+    
+        // return min(fxn(cost, n - 1 ,dp),
+        //            fxn(cost, n - 2,dp));
     }
 };
