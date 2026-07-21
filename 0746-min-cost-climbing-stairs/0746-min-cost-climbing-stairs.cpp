c63f1fac096a1a397856b2af2d1c6caceb9c2a39
Brijesh Gangwar <130545652+Brijesh-gangwar@users.noreply.github.com>
2026-07-22 01:47:08 +0530

    Time: 0 ms (100%), Space: 17.6 MB (52.27%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -14,19 +14,28 @@ public:
     }
 
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
 
-        vector<int> dp(n+1,-1);
-        
-        dp[0] = cost[0];
-        dp[1] = cost[1];
-
-        for(int i = 2 ; i < n;i++){
-            dp[i] = cost[i] +  min( dp[i-1] , dp[i-2] );
-            
+           int a = cost[0];
+        int b = cost[1];
+        for(int i = 2; i < cost.size(); i++){
+            int c = min(a,b) + cost[i];
+            a = b;
+            b = c;
         }
+        return min(a,b);
+        // int n = cost.size();
 
-        return min(dp[n-1] , dp[n-2]);
+        // vector<int> dp(n+1,-1);
+        
+        // dp[0] = cost[0];
+        // dp[1] = cost[1];
+
+        // for(int i = 2 ; i < n;i++){
+        //     dp[i] = cost[i] +  min( dp[i-1] , dp[i-2] );
+            
+        // }
+
+        // return min(dp[n-1] , dp[n-2]);
     
         // return min(fxn(cost, n - 1 ,dp),
         //            fxn(cost, n - 2,dp));
