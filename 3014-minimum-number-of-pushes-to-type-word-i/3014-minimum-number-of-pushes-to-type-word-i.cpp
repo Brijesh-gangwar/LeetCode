fa2f36d8a2a63e60001cd065f885389b4927d84f
Brijesh Gangwar <130545652+Brijesh-gangwar@users.noreply.github.com>
2026-07-30 22:13:50 +0530

    Time: 0 ms (100%), Space: 8.3 MB (54.67%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -8,12 +8,11 @@ public:
         int bIndex = n % 8;
 
         int ans = 0;
-        int weight =  1;
-        for( int i  = 1; i <= nChar ; i++){
+        int weight;
+        for( weight = 1; weight <= nChar ; weight++){
             ans = ans + 8 * weight;
-            weight++;
         }
-
+        
         ans =  ans + bIndex * weight;
 
         return ans;
