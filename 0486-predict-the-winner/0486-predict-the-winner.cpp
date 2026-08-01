5630510cf623f85c13600e8a11e40997d045ec0e
Brijesh Gangwar <130545652+Brijesh-gangwar@users.noreply.github.com>
2026-08-02 00:53:56 +0530

    Time: 183 ms (5.37%), Space: 9.5 MB (96.48%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    bool rec(vector<int>& nums , int score1 , int score2 , int i , int j, int count){
+    bool rec(vector<int>& nums , int netscore , int i , int j, int count){
         // base case
         if(i > j){
-            if(score1 >= score2) return true;
+            if(netscore >= 0) return true;
             else return false;
         }
 
@@ -13,9 +13,9 @@ public:
         if(count %2 == 1){
             
             // taking ith element to add in score
-            ith = rec(nums , score1 + nums[i] , score2, i+1 , j , count+1);
+            ith = rec(nums , netscore  + nums[i] , i+1 , j , count+1);
             // taking jth element to add in score
-            jth = rec(nums , score1 + nums[j] , score2, i , j-1 , count+1);
+            jth = rec(nums , netscore  + nums[j] , i , j-1 , count+1);
 
             return ith || jth;
 
@@ -23,9 +23,9 @@ public:
         // count % 2 == 0 -> player2 take turns
         else{
             // taking ith element to add in score
-            ith = rec(nums , score1 , score2 + + nums[i], i+1 , j, count+1);
+            ith = rec(nums , netscore - nums[i], i+1 , j, count+1);
             // taking jth element to add in score
-            jth = rec(nums , score1 , score2 + nums[j] , i , j-1 , count+1);
+            jth = rec(nums , netscore -  nums[j] , i , j-1 , count+1);
 
             return ith && jth;
         }
@@ -36,6 +36,6 @@ public:
         
         if(nums.size() == 1) return true;
 
-        return rec(nums, 0 , 0 , 0 , nums.size()-1 , 1);
+        return rec(nums, 0 , 0 , nums.size()-1 , 1);
     }
 };
