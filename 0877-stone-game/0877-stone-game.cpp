4871a4a2c9d959571ca6971ac4496eb79d90d5d7
Brijesh Gangwar <130545652+Brijesh-gangwar@users.noreply.github.com>
2026-08-02 23:59:17 +0530

    Time: 8 ms (31.43%), Space: 19.5 MB (27.79%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,26 +1,53 @@
 class Solution {
 public:
-    int rec(vector<int>& piles , int i , int j , vector<vector<int>>& dp){
-        if(i == j) return piles[i];
+    // int rec(vector<int>& piles , int i , int j , vector<vector<int>>& dp){
+    //     if(i == j) return piles[i];
 
-        if(dp[i][j] != -1) return dp[i][j];
+    //     if(dp[i][j] != -1) return dp[i][j];
 
-        int ith = piles[i] - rec(piles , i + 1 , j ,dp);
-        int jth = piles[j] - rec(piles , i , j - 1 ,dp);
+    //     int ith = piles[i] - rec(piles , i + 1 , j ,dp);
+    //     int jth = piles[j] - rec(piles , i , j - 1 ,dp);
 
-        dp[i][j] = max(ith , jth);
+    //     dp[i][j] = max(ith , jth);
 
-        return dp[i][j];
+    //     return dp[i][j];
+    // }
+
+    int tab(vector<int>& piles, int i, int j) {
+        int n = piles.size();
+
+        vector<vector<int>> dp(n ,vector<int>(n,0));
+
+        for(int i = 0; i < n ; i++){
+            for(int j = 0; j < n; j++){
+                if( i == j )
+                    dp[i][i] = piles[i];
+            }
+        }
+
+        for(int i = n-1 ; i >= 0; i--){
+            for(int j = i+1 ; j < n; j++){
+        int ith = piles[i] - dp[i+1][j];
+        int jth = piles[j] - dp[i][j - 1];
+
+        dp[i][j] = max(ith, jth);
+    
+            }
+        }
+
+        return dp[0][n-1];
     }
 
     bool stoneGame(vector<int>& piles) {
         int n = piles.size();
 
-        vector<vector<int>> dp(n ,vector<int>(n,-1));
+        // vector<vector<int>> dp(n ,vector<int>(n,-1));
 
-        int dif = rec(piles , 0 , n-1 , dp);
+        // int dif = rec(piles , 0 , n-1 , dp);
+        int dif = tab(piles, 0, n - 1);
 
-        if(dif > 0) return true;
+        if (dif > 0)
+            return true;
         return false;
     }
 };
