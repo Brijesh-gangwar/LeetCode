fb9fd54cfdc532e892ec2e27a8d2d6117db0b357
Brijesh Gangwar <130545652+Brijesh-gangwar@users.noreply.github.com>
2026-08-03 00:00:29 +0530

    Time: 3 ms (38.73%), Space: 19.7 MB (20.08%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -16,26 +16,21 @@ public:
     int tab(vector<int>& piles, int i, int j) {
         int n = piles.size();
 
-        vector<vector<int>> dp(n ,vector<int>(n,0));
+        vector<vector<int>> dp(n, vector<int>(n, 0));
 
-        for(int i = 0; i < n ; i++){
-            for(int j = 0; j < n; j++){
-                if( i == j )
-                    dp[i][i] = piles[i];
+      for (int i = 0; i < n; i++)
+            dp[i][i] = piles[i];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                int ith = piles[i] - dp[i + 1][j];
+                int jth = piles[j] - dp[i][j - 1];
+
+                dp[i][j] = max(ith, jth);
             }
         }
 
-        for(int i = n-1 ; i >= 0; i--){
-            for(int j = i+1 ; j < n; j++){
-        int ith = piles[i] - dp[i+1][j];
-        int jth = piles[j] - dp[i][j - 1];
-
-        dp[i][j] = max(ith, jth);
-    
-            }
-        }
-
-        return dp[0][n-1];
+        return dp[0][n - 1];
     }
 
     bool stoneGame(vector<int>& piles) {
