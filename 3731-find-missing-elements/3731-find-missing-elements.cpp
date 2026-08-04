67c40964c25c2fc31b4c6fa44e587d06992f42b6
Brijesh Gangwar <130545652+Brijesh-gangwar@users.noreply.github.com>
2026-08-04 22:09:56 +0530

    Time: 0 ms (100%), Space: 33.9 MB (8.08%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -3,9 +3,8 @@ public:
     vector<int> findMissingElements(vector<int>& nums) {
         vector<int>ans;
 
-        sort(nums.begin(),nums.end());
-        int start=nums[0];
-        int end=nums.back();
+        int start = *min_element(nums.begin(), nums.end());
+        int end= *max_element(nums.begin(), nums.end());
 
         set<int>st(nums.begin(),nums.end());
 
