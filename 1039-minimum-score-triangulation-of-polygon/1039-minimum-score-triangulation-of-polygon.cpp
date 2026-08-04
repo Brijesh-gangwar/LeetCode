f6c6d498380b2d555cccbfd88a763dd033db80c2
Brijesh Gangwar <130545652+Brijesh-gangwar@users.noreply.github.com>
2026-08-05 00:50:26 +0530

    Time: 0 ms (100%), Space: 11.6 MB (5.26%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
@@ -1,19 +1,46 @@
 class Solution {
 public:
-    int rec(vector<int>& v, int i, int j, vector<vector<int>>& dp) {
-        if (j - i == 1)
-            return 0;
+    // int recMem(vector<int>& v, int i, int j, vector<vector<int>>& dp) {
+    //     if (j - i == 1)
+    //         return 0;
 
-        if (dp[i][j] != INT_MAX)
-            return dp[i][j];
+    //     if (dp[i][j] != INT_MAX)
+    //         return dp[i][j];
 
-        for (int k = i + 1; k < j; k++) {
-            dp[i][j] = min(dp[i][j], v[i] * v[k] * v[j] + rec(v, i, k, dp) +
-                                         rec(v, k, j, dp));
+    //     for (int k = i + 1; k < j; k++) {
+    //         dp[i][j] = min(dp[i][j], v[i] * v[k] * v[j] + recMem(v, i, k, dp)
+    //         +
+    //                                      recMem(v, k, j, dp));
+    //     }
+
+    //     return dp[i][j];
+    // }
+
+    int tab(vector<int>& v) {
+
+    int n = v.size();
+
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+
+    for (int i = n - 1; i >= 0; i--) {
+
+        for (int j = i + 2; j < n; j++) {
+
+            dp[i][j] = INT_MAX;
+
+            for (int k = i + 1; k < j; k++) {
+
+                dp[i][j] = min(dp[i][j],
+                               v[i] * v[k] * v[j] +
+                               dp[i][k] +
+                               dp[k][j]);
+            }
         }
-
-        return dp[i][j];
     }
+
+    return dp[0][n - 1];
+}
+
     int minScoreTriangulation(vector<int>& values) {
 
         int n = values.size();
@@ -22,6 +49,7 @@ public:
 
         vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
 
-        return rec(values, 0, n - 1, dp);
+        // return recMem(values, 0, n - 1, dp);
+        return tab(values);
     }
 };
