7eb84e5a56d500d047a8b86c8a3cdce826404e95
Brijesh Gangwar <130545652+Brijesh-gangwar@users.noreply.github.com>
2026-08-13 01:27:31 +0530

    Time: 60 ms (73.4%), Space: 149.4 MB (27.77%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -7,21 +7,39 @@ public:
         int ans = 0;
         int i = 0;
 
+        int culprit = 0;
+
         for (int j = 0; j < n; j++) {
             int num = nums[j];
 
             mp[num]++;
 
-            if (mp[num] > k) {
+            // if (mp[num] > k) {
 
-                while (mp[num] > k) {
-                    mp[nums[i]]--;
-                    i++;
+            //     while (mp[num] > k) {
+            //         mp[nums[i]]--;
+            //         i++;
+            //     }
+
+            // } else {
+            //     ans = max(ans, j - i + 1);
+            // }
+
+            if (mp[num] == k + 1) {
+                culprit++;
+            }
+
+            if (culprit > 0) {
+                if (mp[nums[i]] == k+1) {
+                    culprit--;
                 }
 
-            } else {
-                ans = max(ans, j - i + 1);
+                mp[nums[i]]--;
+                i++;
             }
+
+            if (culprit == 0)
+                ans = max(ans, j - i + 1);
         }
         return ans;
     }
