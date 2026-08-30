7bb0bcaced68acff4fcfac8bd33af7f492577521
Brijesh Gangwar <130545652+Brijesh-gangwar@users.noreply.github.com>
2026-08-30 21:57:23 +0530

    Time: 0 ms (100%), Space: 92.2 MB (22.14%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -5,47 +5,23 @@ public:
         if (n == 1 || n == 2)
             return n;
 
-        int maxval = INT_MIN;
-        int maxindex = -1;
+        int minIndex = 0;
+        int maxIndex = 0;
 
-        int minval = INT_MAX;
-        int minindex = -1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[minIndex])
+                minIndex = i;
 
-        for (int i = 0; i < n; i++) {
-            int val = nums[i];
-
-            // max element
-            if (val > maxval) {
-                maxval = val;
-                maxindex = i;
-            }
-
-            // min element
-            if (val < minval) {
-                minval = val;
-                minindex = i;
-            }
+            if (nums[i] > nums[maxIndex])
+                maxIndex = i;
         }
 
-        // i = smaller index btw minindex and maxindex
-        // j = larger index btw minindex and maxindex
-        int i, j;
-        if (minindex < maxindex) {
-            i = minindex;
-            j = maxindex;
-        } else {
-            i = maxindex;
-            j = minindex;
-        }
+        // i = earlier position
+        // j = later position
+        int i = min(minIndex, maxIndex);
+        int j = max(minIndex, maxIndex);
 
-        vector<int> arr;
-        arr.push_back(i + 1);
-        arr.push_back(j - i);
-        arr.push_back(n - j);
-
-        sort(arr.begin(), arr.end());
-
-        // ans = sum of 2 smallest pi
-        return arr[0] + arr[1];
+        // min cost to delete both
+        return min({(j + 1), (n - i), (i + 1 + n - j)});
     }
 };
