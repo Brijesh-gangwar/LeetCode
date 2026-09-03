92b1ac2396d152436cb5cf122e3d128b729718f9
Brijesh Gangwar <130545652+Brijesh-gangwar@users.noreply.github.com>
2026-09-03 23:58:58 +0530

    Time: 3 ms (72.51%), Space: 165.7 MB (87.43%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -5,34 +5,27 @@ public:
         bool alleven = false;
 
         // finding min odd  , checking alleven
-        for(int i : nums1){
-            if(i % 2 != 0 )
+        for (int i : nums1) {
+            if (i % 2 != 0)
                 minodd = min(minodd, i);
-            else 
+            else
                 alleven = true;
         }
 
         // all even
-        if(minodd == INT_MAX) 
+        if (minodd == INT_MAX)
             return true;
 
         // all odd
-        if(alleven == false)
+        if (alleven == false)
             return true;
 
-        // creating mixed nums2
-        vector<int> nums2;
-        for(int i : nums1){
+        for (int i : nums1) {
             // odd -> entry in nums2
-            if(i % 2 != 0)
-                nums2.push_back(i);
-            // even -> flipping parity to odd
-            else{
+            if (i % 2 == 0) {
                 // if nums1[i] - nums1[j] >= 1 -> fails
-                if(i - minodd < 1) 
+                if (i - minodd < 1)
                     return false;
-
-                nums2.push_back(i-minodd);
             }
         }
 
