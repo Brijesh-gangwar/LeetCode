e86e9614a544cb75f5da0407666ab83959ad477a
Brijesh Gangwar <130545652+Brijesh-gangwar@users.noreply.github.com>
2026-09-05 03:06:49 +0530

    Time: 0 ms (100%), Space: 31.2 MB (56.51%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -6,24 +6,22 @@ public:
 
         int n = nums.size();
 
-        // finding minarr 
-        vector<int> minarr(n,-1);
-        for(int i = n - 1; i >= 0; i--){
-            minval = min(minval , nums[i]);
+        // finding minarr -> minarr[i] = min(nums[i..n - 1])
+        vector<int> minarr(n);
+        for (int i = n - 1; i >= 0; i--) {
+            minval = min(minval, nums[i]);
 
             minarr[i] = minval;
         }
 
         int index = 0;
 
-        for(int i : nums){
+        for (int i = 0; i < n; i++) {
             // max(nums[0..i])
-            maxval = max(maxval , i);
-            // min(nums[i..n - 1])
-            minval = minarr[index];
-
-            if((maxval - minval) <= k) return index;
-            index++;
+            maxval = max(maxval, nums[i]);
+             
+            if (maxval - minarr[i] <= k)
+                return i;
         }
         return -1;
     }
