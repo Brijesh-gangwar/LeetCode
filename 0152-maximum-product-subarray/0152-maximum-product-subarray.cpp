25278c1e8e8c6827579aaf71f921f182060b892d
Brijesh Gangwar <130545652+Brijesh-gangwar@users.noreply.github.com>
2026-09-10 01:25:05 +0530

    Time: 4 ms (11.59%), Space: 17.8 MB (60.25%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        
         int maxprod = nums[0];
-int n = nums.size();
-        int iprod = nums[0];
-        int xprod = nums[0];
-        for (int i = 1; i < n; i++) {
-            if (nums[i]>= 0) {
-                iprod = min(nums[i], iprod * nums[i]);
-                xprod = max(nums[i], xprod * nums[i]);
-            }
-            // negative num 
-            else {
-                int temp = xprod;
-                xprod = max(nums[i], iprod * nums[i]);
-                iprod = min(nums[i], temp * nums[i]);
-            }
-            maxprod = max(maxprod , xprod);
+        int n = nums.size();
 
+        int iprod = 1;
+        int xprod = 1;
+
+        for (int i = 0; i < n; i++) {
+            int tempx = xprod;
+            int tempi = iprod;
+
+            xprod = max({nums[i], tempx * nums[i], tempi * nums[i]});
+
+            iprod = min({nums[i], tempx * nums[i], tempi * nums[i]});
+
+            maxprod = max(maxprod, xprod);
         }
 
         return maxprod;
