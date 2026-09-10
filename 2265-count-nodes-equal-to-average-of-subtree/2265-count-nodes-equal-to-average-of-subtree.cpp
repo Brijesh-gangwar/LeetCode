77b42a5de1c5b80cdc4ed8723758a369790e6d92
Brijesh Gangwar <130545652+Brijesh-gangwar@users.noreply.github.com>
2026-09-11 00:02:03 +0530

    Time: 0 ms (100%), Space: 15.7 MB (64.72%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -6,32 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int result = 0;
-    pair<int,int> solve(TreeNode* root) {
-        if (root == nullptr)
-            return {0,0};
+    int ans=0;
+    pair<int, int> solve(TreeNode* root){
+        if(root==NULL) return {0,0};
 
         auto left = solve(root->left);
         auto right = solve(root->right);
 
-        int totalsum = root->val + left.first + right.first;
-        int totalcount = left.second + right.second + 1;
-        
-        if((totalsum/totalcount) == root->val)
-            result += 1;
+        int sum=left.first+right.first+root->val;
+        int count=left.second+right.second+1;
 
-        return {totalsum , totalcount};
+        if(sum/count==root->val){
+            ans++;
+        }
 
+        return {sum, count};
     }
 
     int averageOfSubtree(TreeNode* root) {
-       auto ans = solve(root);
-        return result;
+        solve(root);
+        return ans;
     }
 };
