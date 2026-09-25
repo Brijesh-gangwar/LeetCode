78c9d4c759e6f0d72428250a1c0b6573c308fbce
Brijesh Gangwar <130545652+Brijesh-gangwar@users.noreply.github.com>
2026-09-25 23:06:26 +0530

    Time: 0 ms (100%), Space: 30.9 MB (81.78%) - LeetHub

---
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int digsum(int n){
+        int sum = 0;
+        while(n){
+            sum += n%10;
+            n /=10;
+        }
+        return sum;
+    }
+
+    int smallestIndex(vector<int>& nums) {
+        
+        for(int i = 0; i< nums.size();i++){
+            if(nums[i] < 10 && nums[i] == i){
+                return i;
+            }else if(nums[i] < 10 && nums[i] != i) continue;
+            else {
+                int sum = digsum(nums[i]);
+                if(sum == i) return i;
+            }
+        }
+        return -1;
+    }
+};
